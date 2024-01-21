c05db2b660d01a0347cc4d5aba3940f283575111
bbvv77 <mvc@li.cm>
2024-01-22 02:22:58 +0800

    Update app.js

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ function googleAnalyticsMiddleware(data) {
 }
 
 var unblocker = new Unblocker({
-    prefix: '/web/',
+    prefix: '/webcdn/',
     requestMiddleware: [
         youtube.processRequest
     ],
