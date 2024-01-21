5395d12696a9a578901c66a165e2e293b504b755
bbvv77 <mvc@li.cm>
2024-01-22 03:49:36 +0800

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var querystring = require('querystring');
 var express = require('express');
 var Unblocker = require('unblocker');
 var Transform = require('stream').Transform;
-var youtube = require('unblocker/examples/youtube/youtube.js')
+var youtube = https://raw.githubusercontent.com/bbvv77/node-unblocker/master/examples/youtube/youtube.js;
 
 var app = express();
 
